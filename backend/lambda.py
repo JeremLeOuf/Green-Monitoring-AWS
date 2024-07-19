d2643c098d8d0213fa1196b737dca6ad8cedc0a8
Jerem <109222798+JeremLeOuf@users.noreply.github.com>
2024-07-19 13:40:20 +0200

    Revert "Update lambda for FE clarity"
    This reverts commit 7d5f0513ebe5bcfd6cabc7e29bb5747d2df06a2e.

--- backend/lambda.py
+++ backend/lambda.py
@@ -27,7 +27,6 @@ region_mapping = {
     'sa-east-1': 'BR-CS',
 }
 
-
 def get_carbon_intensity(region):
     api_url = f"https://api.electricitymap.org/v3/carbon-intensity/latest?zone={region_mapping.get(region)}"
     headers = {
@@ -42,19 +41,15 @@ def get_carbon_intensity(region):
     except requests.RequestException as e:
         raise Exception(f"Error fetching carbon intensity: {str(e)}")
 
-
 def lambda_handler(event, context):
     print("Received event:", json.dumps(event))  # Log the event object
 
     try:
         # Extract data from the event payload with default values
         instance_type = event['instance_type']
-        period_label = event.get(
-            'period_label', '24 hours')  # Default: '24 hours'
-        # Default: 86400 seconds (24 hours)
-        period = float(event.get('period', 86400))
-        vcpu_utilization = float(
-            event.get('vcpu_utilization', 10))  # Default: 10%
+        period_label = event.get('period_label', '24 hours')  # Default: '24 hours'
+        period = float(event.get('period', 86400))  # Default: 86400 seconds (24 hours)
+        vcpu_utilization = float(event.get('vcpu_utilization', 10))  # Default: 10%
         region = event['region']
 
         # Load CSV data
@@ -63,28 +58,22 @@ def lambda_handler(event, context):
 
         # Fetch power consumption data for the given instance type
         try:
-            instance_row = instance_data[instance_data['Instance type']
-                                         == instance_type].iloc[0]
+            instance_row = instance_data[instance_data['Instance type'] == instance_type].iloc[0]
             min_watts = float(instance_row['PkgWatt @ Idle'].replace(",", "."))
             max_watts = float(instance_row['PkgWatt @ 100%'].replace(",", "."))
         except (KeyError, IndexError) as e:
-            raise Exception(
-                f"Instance type '{instance_type}' not found or data missing in CSV") from e
+            raise Exception(f"Instance type '{instance_type}' not found or data missing in CSV") from e
 
         # Calculate average watts, hours in period, and kilowatt-hours
-        avg_watts = min_watts + \
-            (vcpu_utilization / 100.0) * (max_watts - min_watts)
+        avg_watts = min_watts + (vcpu_utilization / 100.0) * (max_watts - min_watts)
         hours_in_period = period / 3600.0
         kWh = avg_watts * hours_in_period / 1000.0  # Convert from Wh to kWh
 
         # Get carbon intensity for the region
         carbon_intensity = get_carbon_intensity(region)
 
-        # Define PUE for AWS:
-        PUE = 1.135
-
         # Calculate estimated CO2 emissions
-        estimated_co2e = kWh * carbon_intensity * PUE
+        estimated_co2e = kWh * carbon_intensity
 
         # Determine the appropriate period label based on the input period
         if period >= 31536000:  # 1 year in seconds
@@ -92,17 +81,13 @@ def lambda_handler(event, context):
         elif period >= 2592000:  # 30 days in seconds
             period_label = '1 month'
         elif period >= 604800:  # 1 week in seconds
-            period_label = f"{int(period // 604800)} week" if int(period //
-                                                                  604800) == 1 else f"{int(period // 604800)} weeks"
+            period_label = f"{int(period // 604800)} week" if int(period // 604800) == 1 else f"{int(period // 604800)} weeks"
         elif period >= 86400:  # 1 day in seconds
-            period_label = f"{int(period // 86400)} day" if int(period //
-                                                                86400) == 1 else f"{int(period // 86400)} days"
+            period_label = f"{int(period // 86400)} day" if int(period // 86400) == 1 else f"{int(period // 86400)} days"
         elif period >= 3600:  # 1 hour in seconds
-            period_label = f"{int(period // 3600)} hour" if int(period //
-                                                                3600) == 1 else f"{int(period // 3600)} hours"
+            period_label = f"{int(period // 3600)} hour" if int(period // 3600) == 1 else f"{int(period // 3600)} hours"
         else:
-            period_label = f"{int(period // 60)} minute" if int(period //
-                                                                60) == 1 else f"{int(period // 60)} minutes"
+            period_label = f"{int(period // 60)} minute" if int(period // 60) == 1 else f"{int(period // 60)} minutes"
 
         # Create separate dictionaries for each table's data
         min_max_data = [
@@ -111,21 +96,18 @@ def lambda_handler(event, context):
         ]
         avg_watt_hours_data = [
             {'Metric': 'Average Watts:', 'Value': f'{avg_watts:.2f} W'},
-            {'Metric': f'Power consumption (kWh) for {period_label}:',
-             'Value': f'{kWh:.2f} kWh'}
+            {'Metric': f'Power consumption (kWh) for {period_label}:', 'Value': f'{kWh:.2f} kWh'}
         ]
         carbon_intensity_data = [
-            {'Metric': 'Carbon Intensity (gCO2e/kWh):',
-             'Value': f'{carbon_intensity:.0f} gCO2e/kWh'}
+            {'Metric': 'Carbon Intensity (gCO2e/kWh):', 'Value': f'{carbon_intensity:.0f} gCO2e/kWh'}
         ]
         co2e_emissions_data = [
-            {'Metric': 'Estimated CO2e Emissions:',
-             'Value': f'<b>{estimated_co2e:,.0f} gCO2e</b>'.replace(",", ".")}
+            {'Metric': 'Estimated CO2e Emissions:', 'Value': f'{estimated_co2e:.2f} gCO2e'}
         ]
 
         # Create the messages for the response
         messages = [
-            f"Your {instance_type} instance with an average {vcpu_utilization:.0f}% CPU Utilization over a period of {period_label} would generate an average of {avg_watts:.2f} Watts (W), consuming a total of <b>{kWh:.2f} Kilowatt-Hours (kWh)</b> over that period.<br><br>The carbon intensity for your region is {carbon_intensity:.2f} gCO2/kWh as of today."]
+            f"Your {instance_type} instance with an average {vcpu_utilization:.0f}% CPU Utilization over a period of {period_label} would generate an average of {avg_watts:.2f} Watts (W), consuming a total of <b>{kWh:.2f} Kilowatt-Hours (kWh)</b> over that period.<br><br>The carbon intensity for your region is {carbon_intensity:.2f} gCO2/kWh (as of **TODO**)."]
 
         # Combine results into a dictionary
         results = {
